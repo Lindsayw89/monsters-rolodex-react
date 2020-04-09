a28788c3780f9e9f300ea1a84a0764c04574ec98
lindsay wilson <lwilson@germblast.com>
2020-04-09 14:56:18 -0500

    Updates

---
+++ precache-manifest.e3755a72065f6c6850c82869ca1978fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8802ca2584f18a9dfc2f9b86fa29d69e",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "fb7b78aa9c3f28846b95",
+    "url": "/monsters-rolodex-react/static/css/main.dc98ca5c.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7b78aa9c3f28846b95",
+    "url": "/monsters-rolodex-react/static/js/main.c6310527.chunk.js"
+  },
+  {
+    "revision": "18908f6eb88b51269a6a",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b428d943.js"
+  }
+]);
